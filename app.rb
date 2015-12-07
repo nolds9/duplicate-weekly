87905e1e7e319fd3579bbee91720092876d09ec0
Nicholas Olds <nolds9@yahoo.com>
2015-12-07 16:35:51 -0500

    get request for discover weekly playlist

--- app.rb
+++ app.rb
@@ -3,17 +3,28 @@ require 'rest-client'
 require_relative "env.rb"
 
 class Spotify
-  # include HTTParty
+  include HTTParty
+  attr_accessor :user, :playlists
   def initialize(token)
-      headers = {
-        "Authorization" => "Bearer #{token}",
-        "Accept" => "application/json"
-     }
-      puts headers
-     response = RestClient.get("https://api.spotfiy.com/vi/me")
+    @headers = {
+      "Authorization" => "Bearer #{token}",
+      "Accept" => "application/json"
+    }
+    response = HTTParty.get("https://api.spotify.com/v1/me", headers: @headers)
+    @user = response
   end
 
+  def playlists
+    response = HTTParty.get("https://api.spotify.com/v1/me/playlists", headers: @headers)
+    @playlists = response["items"]
+  end
+
+  def discover_weekly
+    self.playlists.select { |playlist|
+      playlist["name"] == "Discover Weekly"
+    }[0]
+  end
 end
 
 s = Spotify.new(SPOTIFY_TOKEN)
-p s
+p s.discover_weekly
